dcd60af6d86864f2e9d3df7d10e5628f6b13a21e
Alexei <48149971+AlexeSemenenko@users.noreply.github.com>
2019-10-23 22:22:15 +0300

    Add files via upload

--- CanonicalForm/main.cpp
+++ CanonicalForm/main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
 
+int main() {
+    std::ifstream in("input.txt");
+    int count_of_vertex = 0;
+    int parent = 0;
+    int child = 0;
+
+    in >> count_of_vertex;
+
+    std::vector<int> family(count_of_vertex, 0);
+
+    while (!in.eof()) {
+        in >> parent;
+        in >> child;
+        family[child - 1] = parent;
+    }
+
+    in.close();
+
+    std::ofstream out("output.txt");
+    for (int i = 0; i < count_of_vertex; i++){
+        out << family[i] << " ";
+    }
+
+    out.close();
+
+    return 0;
+}
