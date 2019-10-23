d9c0c2dd6d51cab86fe9cd96036c67c0184ae8a2
Alexei <48149971+AlexeSemenenko@users.noreply.github.com>
2019-10-23 22:20:32 +0300

    Add files via upload

--- PowerSequence/main.cpp
+++ PowerSequence/main.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
+int main() {
+    std::ifstream in("input.txt");
+    int count_of_vertex = 0;
+    int count_of_edge = 0;
+    int tmp = 0;
+
+    in >> count_of_vertex;
+    in >> count_of_edge;
+
+    std::vector<int> power(count_of_vertex);
+
+    while (!in.eof()) {
+        in >> tmp;
+        power[tmp - 1]++;
+    }
+
+    std::sort(power.rbegin(), power.rend());
+
+    in.close();
+
+    std::ofstream out("output.txt");
+    for (int i = 0; i < count_of_vertex; i++){
+        out << power[i] << " ";
+    }
+
+    out.close();
+
+    return 0;
+}
